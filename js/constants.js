73c798d190e88e6f4fdbd0ab067989922dde9a56
dungameo0204 <133087950+dungameo0204@users.noreply.github.com>
2026-01-01 13:00:27 +0700

    add more time1

--- js/constants.js
+++ js/constants.js
@@ -4,7 +4,7 @@ const CONFIG = {
     CELL_SIZE: 50, // Vẫn giữ để tính toán trên PC, mobile dùng CSS đè
     
     // Cấu hình độ khó
-    BASE_TIME: 80,      // Thời gian cơ bản (giây) cho Level 1
+    BASE_TIME: 90,      // Thời gian cơ bản (giây) cho Level 1
     TIME_DECREASE: 5,   // Mỗi level giảm 5 giây
     MIN_TIME: 15,       // Thời gian tối thiểu không được thấp hơn mức này
     SCORE_PER_MATCH: 100,
